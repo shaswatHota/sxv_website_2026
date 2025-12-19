1de3ace2285f5203cd7d22f9e1701298b081b801
piyushmishra06 <piyushmishra0620@gmail.com>
2025-12-19 18:57:00 +0530

    Fixed password validation in login page UI

--- Schemas/loginSchema.ts
+++ Schemas/loginSchema.ts
@@ -2,7 +2,9 @@ import {z} from "zod";
 
 export const LoginSchema = z.object({
     email : z.string().nonempty("Email is required").email("Invalid email address"),
-    password : z.string().nonempty("Password is required").min(8,{message:"Password must be atleast 8 characters long"}).regex(/^\d+$/, "Phone number should only contain digits.")
+    password: z.string().nonempty("Password is required").min(8,{message:"Password must be atleast 8 characters long."}).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/, {
+    message: "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
+  })
 });
 
 export type LoginFormType = z.infer<typeof LoginSchema>; 
