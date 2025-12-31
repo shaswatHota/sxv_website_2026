070465a48c43720f3448b509d1a4a97694dbf9e3
shaswatHota <shaswathota04@gmail.com>
2025-12-31 21:04:43 +0530

    baseUrl fix

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sxv-backend-eight.vercel.app/",
+  baseURL: "https://sxv-backend-eight.vercel.app",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json"
